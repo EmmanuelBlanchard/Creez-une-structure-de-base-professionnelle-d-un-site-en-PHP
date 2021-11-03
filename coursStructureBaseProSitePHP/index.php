d0aa85438e9930c53a2135205fc570cbe23e44c7
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-11-03 15:22:11 +0100

    1-Les étapes de la création du site standard, Etape 2 : Création du template de base

--- coursStructureBaseProSitePHP/index.php
+++ coursStructureBaseProSitePHP/index.php
@@ -1,1 +1,5 @@
-Coucou
+<?php
+$page_description = "Description de la page d'accueil";
+$page_title = "Titre de la page d'accueil";
+$page_content = "<h1>Coucou</h1>";
+require_once("views/common/template.php");
